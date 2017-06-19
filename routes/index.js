04ef63a187db5c41ec4a6daf3928db37fe560322
Adam Herstein <adam.herstein@icloud.com>
2017-06-19 10:39:08 -0700

    Added userlist method to pull data from mongo.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
 /* GET Hello World page. */
 router.get('/helloworld', function (req, res) {
     res.render('helloworld', {title: 'Hello, World!'});
 });
+/* GET Userlist page. */
+router.get('/userlist', function (req, res) {
+    const db = req.db;
+    const collection = db.get('usercollection');
+    collection.find({}, {}, function (e, docs) {
+        res.render('userlist', {
+            "userlist": docs
+        });
+    });
+});
+
 
 module.exports = router;
