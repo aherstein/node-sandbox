c2942e1617ec0adf6f5b47d9b4ee38eec03abdf4
Adam Herstein <adam.herstein@icloud.com>
2017-06-19 14:03:13 -0700

    Added a post user page.

--- routes/index.js
+++ routes/index.js
@@ -23,4 +23,39 @@ router.get('/userlist', function (req, res) {
     });
 });
 
+/* GET New User page. */
+router.get('/newuser', function (req, res) {
+    res.render('newuser', {title: 'Add New User'});
+});
+
+
+/* POST to Add User Service */
+router.post('/adduser', function (req, res) {
+
+    // Set our internal DB variable
+    const db = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    let userName = req.body.username;
+    let userEmail = req.body.useremail;
+
+    // Set our collection
+    const collection = db.get('usercollection');
+
+    // Submit to the DB
+    collection.insert({
+        "username": userName,
+        "email": userEmail
+    }, function (err, doc) {
+        if (err) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
 module.exports = router;
